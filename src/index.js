b965e818b0407af378f9298a4952611c99dfd901
shuri12290 <120463640+shuri12290@users.noreply.github.com>
2023-01-12 19:18:41 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,70 @@
+/**
+ * const let
+ */
+
+//  var val1 = "var変数";
+//  console.log(val1);
+
+//  // var変数は上書き可能
+
+//  val1 = "var変数を上書き";
+//  console.log(val1);
+
+//  // var変数は再宣言可能
+//  var val1 = "var変数を再宣言";
+//  console.log(val1);
+
+// const val3 = "constへんすう"
+// console.log(val3)
+
+// //const変数は上書き不可
+// val3 = "const変数を上書き"
+
+// const val3 =  "constを再宣言"
+
+//const で定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "taku",
+//   age:24,
+// };
+// val4.name = "takumi";
+// val4.address = "toyama"
+// console.log(val4);
+
+/**
+ *  テンプレート文字列
+ */
+
+//  const name = "たくみ";
+//  const age = 24;
+//  //「私の名前はたくみです。年齢は24です
+
+//  // 従来の方法
+//  const message1 = "わたしのなまえは" + name + "です。年齢は" + age + "です";
+//  console.log(message1)
+
+//  // テンプレート文字列をもちいる
+// const message2 = `私の名前は${name}です。年齢は${age}です`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+// //従来の関数
+// function func1(str){
+//   return str;
+// }
+// const func1 = function(str) {
+//   return str;
+// }
+// console.log(func1("func1です"));
+
+// // アロー関数
+// const func2 = (str) => {
+//   return str;
+// };
+
+// console.log(func2("func2です"));
+
+// const func3 = (num1, num2) =>  num1 + num2;
+// console.log(func3(10,20));
